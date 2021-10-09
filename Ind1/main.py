633344cda4c66017913ead620e00b05908725354
Setet <Franklin03res@gmail.com>
2021-10-09 14:33:07 +0300

    v1.02

--- Ind1/main.py
+++ Ind1/main.py
@@ -1,32 +1,29 @@
-import random
-
-
 def Main():
     Lifting_height = 0
     Numbers = []
-    Aboba = []
-    d=1
-    s1=0
+    List = []
+    d = 1
+    s1 = 0
+    print("Вводите числа через строку\n")
     while True:
-        if d>s1:
+        if d > s1:
             Numbers.append(int(input()))
-            if Numbers[d-1] == 0:
+            if Numbers[d - 1] == 0:
                 break
             else:
                 d += 1
-    print("Полученно "+str(d)+" чисел.")
+    print("Полученно " + str(d) + " чисел.")
     Numbers[-1] = 0
-    print(Numbers)
 
-    for i in range(len(Numbers)-1):
+    for i in range(len(Numbers) - 1):
         if Numbers[i] < Numbers[i + 1]:
             Lifting_height += Numbers[i + 1] - Numbers[i]
         else:
-            Aboba.append(Lifting_height)
+            List.append(Lifting_height)
             Lifting_height = 0
-    print(Aboba)
 
-    max_number = max(Aboba)
+    max_number = max(List)
     print("Наибольшеая высота подъёма:", max_number)
+
 
 Main()
